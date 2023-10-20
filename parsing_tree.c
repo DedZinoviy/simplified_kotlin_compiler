e9fda4a779847594371b6ebe4b6229a15675aaee
Zinoviy Kuzmitch <dima-bolgov.02@mail.ru>
2023-10-20 20:06:43 +0300

    Созданы заготовки функций создания узлов базовых арифметических операций.

--- parsing_tree.c
+++ parsing_tree.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+
 enum ExpressionType
 {
     PLUS,
@@ -16,6 +18,56 @@ struct ExpressionNode
     struct ExpressionNode * left;
     struct ExpressionNode * right;
 };
+
+/*! Создать узел оператора сложения (+).
+\param[in] leftOperand указатель на левый операнд - экземаляр ExpressionNode.
+\param[in] rightOperand указатель на правый операнд - экземаляр ExpressionNode.
+\return указатель на созданный экземпляр узла сложения.
+*/
+struct ExpressionNode * createPlusExpressionNode(struct ExpressionNode * leftOperand, struct ExpressionNode * rightOperand)
+{
+    return NULL;
+}
+
+/*! Создать узел оператора вычитания (-).
+\param[in] leftOperand указатель на левый операнд - экземаляр ExpressionNode.
+\param[in] rightOperand указатель на правый операнд - экземаляр ExpressionNode.
+\return указатель на созданный экземпляр узла вычитания.
+*/
+struct ExpressionNode * createMinusExpressionNode(struct ExpressionNode * leftOperand, struct ExpressionNode * rightOperand)
+{
+    return NULL;
+}
+
+/*! Создать узел оператора умножения (*).
+\param[in] leftOperand указатель на левый операнд - экземаляр ExpressionNode.
+\param[in] rightOperand указатель на правый операнд - экземаляр ExpressionNode.
+\return указатель на созданный экземпляр узла умножения.
+*/
+struct ExpressionNode * createMulExpressionNode(struct ExpressionNode * leftOperand, struct ExpressionNode * rightOperand)
+{
+    return NULL;
+}
+
+/*! Создать узел оператора деления (/).
+\param[in] leftOperand указатель на левый операнд - экземаляр ExpressionNode.
+\param[in] rightOperand указатель на правый операнд - экземаляр ExpressionNode.
+\return указатель на созданный экземпляр узла деления.
+*/
+struct ExpressionNode * createDivExpressionNode(struct ExpressionNode * leftOperand, struct ExpressionNode * rightOperand)
+{
+    return NULL;
+}
+
+/*! Создать узел оператора получния остатка от деления (%).
+\param[in] leftOperand указатель на левый операнд - экземаляр ExpressionNode.
+\param[in] rightOperand указатель на правый операнд - экземаляр ExpressionNode.
+\return указатель на созданный экземпляр узла остатка от деления.
+*/
+struct ExpressionNode * createModExpressionNode(struct ExpressionNode * leftOperand, struct ExpressionNode * rightOperand)
+{
+    return NULL;
+}
 
 struct DoWhileStatementNode
 {
