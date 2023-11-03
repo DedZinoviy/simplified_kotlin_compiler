2000b4b3e345fd204f43b076d0d2f1593e3fbef0
Zinoviy Kuzmitch <dima-bolgov.02@mail.ru>
2023-11-03 13:13:23 +0300

    Добавлены шаблоны функций для генерации DOT кода из узлов Statement, StatementList, Expression, ExpressionList.

---
+++ print_functions.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include "nodes.h"
+
+/*! Сгегнерировать строку в DOT-формате для дальнейшей визуализации для узла Expression.
+\param[in] node Визуализироваемый узел.
+\return Строка кода на языке DOT из узла Expression.
+*/
+char * generateDotFromExpression(struct ExpressionNode * node)
+{
+    return NULL;
+}
+
+/*! Сгегнерировать строку в DOT-формате для дальнейшей визуализации для узла ExpressionList.
+\param[in] listNode Визуализироваемый узел.
+\return Строка кода на языке DOT из узла ExpressionList.
+*/
+char * generateDotFromExpressionList(struct ExpressionListNode * listNode)
+{
+    return NULL;
+}
+
+
+/*! Сгегнерировать строку в DOT-формате для дальнейшей визуализации для узла Statement.
+\param[in] stmt Визуализироваемый узел.
+\return Строка кода на языке DOT из узла Statement.
+*/
+char * generateDotFromStatement(struct StatementNode * stmt)
+{
+    return NULL;
+}
+
+/*! Сгегнерировать строку в DOT-формате для дальнейшей визуализации для узла StatementList.
+\param[in] stmtList Визуализироваемый узел.
+\return Строка кода на языке DOT из узла StatementList.
+*/
+char * generateDotFromStatementList(struct StatementistNode * stmtList)
+{
+    return NULL;
+}
