faf6b76074eb69023e288618d5a5df00527f143b
Zinoviy Kuzmitch <dima-bolgov.02@mail.ru>
2023-11-11 13:38:10 +0300

    Добавлена проверка входных параметров для консольного приложения лексера.

--- skc/main.c
+++ skc/main.c
@@ -2,6 +2,12 @@
 
 int main(int argc, char * argv[])
 {
+	if (argc < 2)
+	{
+		printf("The path to the input file is missing.\n");
+		return -1;
+	}
+
 	printf("Simpified Kotlin Compiler Project.\n");
 	return 0;
 }
