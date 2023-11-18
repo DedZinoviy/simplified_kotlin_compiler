0ff5d2fa81b2ae16c3f5392422aaf606f2a447c0
Zinoviy Kuzmitch <dima-bolgov.02@mail.ru>
2023-11-18 10:13:50 +0300

    Добавлено документирование структур узлов.

--- skc/nodes.h
+++ skc/nodes.h
@@ -1,5 +1,6 @@
 /*------------------------------------ Expression -------------------------------------*/
 
+/*! \brief Перечисление типов Expression. */
 enum ExpressionType
 {
     IDENTIFIER,
@@ -23,31 +24,62 @@ enum ExpressionType
     METHOD_ACCESS
 };
 
+/*! \brief Структура узла Expression. */
 struct ExpressionNode
 {
+    /// Идентификатор узла.
     int id;
+
+    /// Целочисленное значение (только для целочисленных констант).
     int intValue;
+
+    /// Логичское (булевое) значение (только для логических констант).
     int boolValue;
+
+    /// Значение числа с плавающей точкой двойной точности (только для контант типа DOUBLE).
     double doubleValue;
+
+    /// Строка идентификатора (только для функций, методов и переменных).
     char* identifierString;
+
+    /// Тип обозреваемого Expression - вариант перечисления.
     enum ExpressionType type;
+
+    /// Указатель на левый операнд бинарного Expression (для бинарных выражений).
     struct ExpressionNode * left;
+
+    /// Указатель на правый операнд бинарного Expression (для бинарных выражений).
     struct ExpressionNode * right;
+
+    /// Указатель на следующий Expression в списке ExpressionList (использовать при работе со списком Expression).
     struct ExpressionNode * next;
+
+    /// Указатель на список Expression (параметры при вызове функций и методов).
     struct ExpressionListNode * params;  
 };
 
+
+
 /*------------------------------------ ExpressionList -------------------------------------*/
 
+/*! \brief Структура узла списка Expression. */
 struct ExpressionListNode
 {
+    /// Идентификатор узла.
     int id;
+
+    /// Указатель на первый элемент списка Expression.
     struct ExpressionNode * first;
+
+    /// Указатель на последний элемент списка Expression.
     struct ExpressionNode * last;
 };
 
+
+
 /*------------------------------------ Statement -------------------------------------*/
 
+/*! \brief Перечисление типов Statement. */
 enum StatementType
 {
     EXPRESSION,
@@ -61,24 +93,48 @@ enum StatementType
 
 struct StatementListNode;
 
+/*! \brief Структура узла Statement. */
 struct StatementNode
 {
+    /// Идентификатор узла.
     int id;
+
+    /// Тип обозреваемого Statement - вариант перечисления.
     enum StatementType type;
+
+    /// Идентификатор переменной для выражения объявления переменной.
     char * varValId;
+
+    /// Тип переменной для выражения объявления переменной с явным указанием типа.
     char * varValType;
+
+    /// Ссылка на Expression, которое используется при созании Statement.
     struct ExpressionNode * expression;
+
+    /// Ссылка на выражение условия (для циклов WHILE и DO..WHILE). 
     struct ExpressionNode * condition;
+
+    /// Ссылка на простое тело цикла.
     struct StatementNode * singleBody;
+
+    /// Ссылка на составное тело цикла.
     struct StatementListNode * complexBody;
+
+    /// Указатель на следующий Statement в списке StatementList (использовать при работе со списком Statement).
     struct StatementNode * next;
 };
 
 /*------------------------------------ StatementList -------------------------------------*/
 
+/*! \brief Структура узла списка Statement. */
 struct StatementListNode
 {
+    /// Идентификатор узла.
     int id;
+
+    /// Указатель на первый элемент списка Statement.
     struct StatementNode * first;
+
+    /// Указатель на последний элемент списка Statement.
     struct StatementNode * last;
 };
