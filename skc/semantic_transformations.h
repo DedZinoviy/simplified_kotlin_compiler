0c78f035e55a902ab96bcc027b2990b769e7e9b6
Zinoviy Kuzmitch <dima-bolgov.02@mail.ru>
2023-12-04 22:59:01 +0300

    Добавлен шаблон функции замены модифицирующих присваиваний на комбинацию из арифм. операции и присваивания.

--- skc/semantic_transformations.h
+++ skc/semantic_transformations.h
@@ -4,4 +4,9 @@
 /*! Заменить узлы непосредственных литералов на комбинацию из создания объекта и доступа к полю _value этого класса.
 * \param[in,out] root дерево программы - указатель на узел KotlinFile.
 */
-void replaceLiteralsToobjects(struct KotlinFileNode * root);
+void replaceLiteralsToObjects(struct KotlinFileNode * root);
+
+/*! Заменить узлы модифицирующих присваиваний на кобинацию из арифметиеской операции и присваивания.
+* \param[in,out] root дерево программы - указатель на узел KotlinFile.
+*/
+void replaceModifyingAssignments(struct KotlinFileNode * root);
