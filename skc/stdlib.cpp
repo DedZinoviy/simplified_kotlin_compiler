e14813c4eee031f8d49f63068dfa541e7894c847
Zinoviy Kuzmitch <dima-bolgov.02@mail.ru>
2024-01-23 16:00:36 +0300

    Исправлено наименование классов в инициализаторе RTL.

--- skc/stdlib.cpp
+++ skc/stdlib.cpp
@@ -4,71 +4,71 @@ void initStdClasses()
 {
     Type * retVal = NULL;
     /* ----- Инициализация класса Int ------ */
-    ClassTable::items["Int"] = new ClassTableElement();
-    ClassTable::items["Int"]->clsName = "JavaRTL/Int";
-    ClassTable::items["Int"]->isOpen = 0;
+    ClassTable::items["JavaRTL/Int"] = new ClassTableElement();
+    ClassTable::items["JavaRTL/Int"]->clsName = "JavaRTL/Int";
+    ClassTable::items["JavaRTL/Int"]->isOpen = 0;
 
         /* ---------- Добавление конструктора ---------- */
-        ClassTable::items["Int"]->methods->methods["<init>"] = std::map<std::string, class MethodTableElement*>();
+        ClassTable::items["JavaRTL/Int"]->methods->methods["<init>"] = std::map<std::string, class MethodTableElement*>();
         retVal = new Type();
-        retVal->className = ClassTable::items["Int"]->clsName;
+        retVal->className = ClassTable::items["JavaRTL/Int"]->clsName;
         retVal->typ = TypeType::_CLS;
-        ClassTable::items["Int"]->methods->methods["<init>"]["(I)"] = new MethodTableElement(0,0,"<init>","(I)V", NULL, retVal, std::vector<FuncParam>());
+        ClassTable::items["JavaRTL/Int"]->methods->methods["<init>"]["(I)"] = new MethodTableElement(0,0,"<init>","(I)V", NULL, retVal, std::vector<FuncParam>());
 
         /* ---------- Добавление метода сложения ---------- */
-        ClassTable::items["Int"]->methods->methods["plus"] = std::map<std::string, class MethodTableElement*>();
+        ClassTable::items["JavaRTL/Int"]->methods->methods["plus"] = std::map<std::string, class MethodTableElement*>();
         retVal = new Type();
-        retVal->className = ClassTable::items["Int"]->clsName;
+        retVal->className = ClassTable::items["JavaRTL/Int"]->clsName;
         retVal->typ = TypeType::_CLS;
-        ClassTable::items["Int"]->methods->methods["plus"]["(LJavaRTL/Int;)"] = new MethodTableElement(0,0,"plus","(LJavaRTL/Int;)LJavaRTL/Int;", NULL, retVal, std::vector<FuncParam>());
+        ClassTable::items["JavaRTL/Int"]->methods->methods["plus"]["(LJavaRTL/Int;)"] = new MethodTableElement(0,0,"plus","(LJavaRTL/Int;)LJavaRTL/Int;", NULL, retVal, std::vector<FuncParam>());
 
         /* ---------- Добавление метода вычитания ---------- */
-        ClassTable::items["Int"]->methods->methods["minus"] = std::map<std::string, class MethodTableElement*>();
+        ClassTable::items["JavaRTL/Int"]->methods->methods["minus"] = std::map<std::string, class MethodTableElement*>();
         retVal = new Type();
-        retVal->className = ClassTable::items["Int"]->clsName;
+        retVal->className = ClassTable::items["JavaRTL/Int"]->clsName;
         retVal->typ = TypeType::_CLS;
-        ClassTable::items["Int"]->methods->methods["minus"]["(LJavaRTL/Int;)"] = new MethodTableElement(0,0,"minus","(LJavaRTL/Int;)LJavaRTL/Int;", NULL, retVal, std::vector<FuncParam>());
+        ClassTable::items["JavaRTL/Int"]->methods->methods["minus"]["(LJavaRTL/Int;)"] = new MethodTableElement(0,0,"minus","(LJavaRTL/Int;)LJavaRTL/Int;", NULL, retVal, std::vector<FuncParam>());
 
                 /* ---------- Добавление метода умножения ---------- */
-        ClassTable::items["Int"]->methods->methods["times"] = std::map<std::string, class MethodTableElement*>();
+        ClassTable::items["JavaRTL/Int"]->methods->methods["times"] = std::map<std::string, class MethodTableElement*>();
         retVal = new Type();
-        retVal->className = ClassTable::items["Int"]->clsName;
+        retVal->className = ClassTable::items["JavaRTL/Int"]->clsName;
         retVal->typ = TypeType::_CLS;
-        ClassTable::items["Int"]->methods->methods["times"]["(LJavaRTL/Int;)"] = new MethodTableElement(0,0,"times","(LJavaRTL/Int;)LJavaRTL/Int;", NULL, retVal, std::vector<FuncParam>());
+        ClassTable::items["JavaRTL/Int"]->methods->methods["times"]["(LJavaRTL/Int;)"] = new MethodTableElement(0,0,"times","(LJavaRTL/Int;)LJavaRTL/Int;", NULL, retVal, std::vector<FuncParam>());
         
                 /* ---------- Добавление метода деления ---------- */
-        ClassTable::items["Int"]->methods->methods["div"] = std::map<std::string, class MethodTableElement*>();
+        ClassTable::items["JavaRTL/Int"]->methods->methods["div"] = std::map<std::string, class MethodTableElement*>();
         retVal = new Type();
-        retVal->className = ClassTable::items["Int"]->clsName;
+        retVal->className = ClassTable::items["JavaRTL/Int"]->clsName;
         retVal->typ = TypeType::_CLS;
-        ClassTable::items["Int"]->methods->methods["div"]["(LJavaRTL/Int;)"] = new MethodTableElement(0,0,"div","(LJavaRTL/Int;)LJavaRTL/Int;", NULL, retVal, std::vector<FuncParam>());
+        ClassTable::items["JavaRTL/Int"]->methods->methods["div"]["(LJavaRTL/Int;)"] = new MethodTableElement(0,0,"div","(LJavaRTL/Int;)LJavaRTL/Int;", NULL, retVal, std::vector<FuncParam>());
 
 
 
     /* ----- Инициализация класса Char ------ */
-    ClassTable::items["Char"] = new ClassTableElement();
-    ClassTable::items["Char"]->clsName = "JavaRTL/Char";
-    ClassTable::items["Char"]->isOpen = 0;
+    ClassTable::items["JavaRTL/Char"] = new ClassTableElement();
+    ClassTable::items["JavaRTL/Char"]->clsName = "JavaRTL/Char";
+    ClassTable::items["JavaRTL/Char"]->isOpen = 0;
 
     /* ----- Инициализация класса String ------ */
-    ClassTable::items["String"] = new ClassTableElement();
-    ClassTable::items["String"]->clsName = "JavaRTL/String";
-    ClassTable::items["String"]->isOpen = 0;
+    ClassTable::items["JavaRTL/String"] = new ClassTableElement();
+    ClassTable::items["JavaRTL/String"]->clsName = "JavaRTL/String";
+    ClassTable::items["JavaRTL/String"]->isOpen = 0;
 
     /* ----- Инициализация класса Boolean ------ */
-    ClassTable::items["Boolean"] = new ClassTableElement();
-    ClassTable::items["Boolean"]->clsName = "JavaRTL/Boolean";
-    ClassTable::items["Boolean"]->isOpen = 0;
+    ClassTable::items["JavaRTL/Boolean"] = new ClassTableElement();
+    ClassTable::items["JavaRTL/Boolean"]->clsName = "JavaRTL/Boolean";
+    ClassTable::items["JavaRTL/Boolean"]->isOpen = 0;
 
     /* ----- Инициализация класса Any ------ */
-    ClassTable::items["Any"] = new ClassTableElement();
-    ClassTable::items["Any"]->clsName = "JavaRTL/Any";
-    ClassTable::items["Any"]->isOpen = 1;
+    ClassTable::items["JavaRTL/Any"] = new ClassTableElement();
+    ClassTable::items["JavaRTL/Any"]->clsName = "JavaRTL/Any";
+    ClassTable::items["JavaRTL/Any"]->isOpen = 1;
 
     /* ----- Инициализация класса Unit ------ */
-    ClassTable::items["Unit"] = new ClassTableElement();
-    ClassTable::items["Unit"]->clsName = "JavaRTL/Unit";
-    ClassTable::items["Unit"]->isOpen = 0;
+    ClassTable::items["JavaRTL/Unit"] = new ClassTableElement();
+    ClassTable::items["JavaRTL/Unit"]->clsName = "JavaRTL/Unit";
+    ClassTable::items["JavaRTL/Unit"]->isOpen = 0;
 
     /* ----- Инициализация функций ввода-вывода ------ */
 }
