a0d65b5f63ae10076e630dec89b4c763376b69c5
Zinoviy Kuzmitch <dima-bolgov.02@mail.ru>
2024-01-26 00:57:53 +0300

    Добалены вспомогательные обработчики для генерации кода.

---
+++ skc/generation_handler.cpp
@@ -0,0 +1,21 @@
+#include "generation_handler.hpp"
+
+std::vector<char> iload(int num)
+{
+
+}
+
+std::vector<char> aload(int num)
+{
+    
+}
+
+std::vector<char> istore(int num)
+{
+
+}
+
+std::vector<char> astore(int num)
+{
+    
+}
